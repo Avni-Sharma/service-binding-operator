3d1ebe9ab961c045875c7d97b19d028fdd8128e0
Avni Sharma <16avnisharma@gmail.com>
2020-05-21 16:45:33 +0530

    Initial data structure for bindingInfo

--- pkg/controller/servicebindingrequest/annotations/bindinginfo.go
+++ pkg/controller/servicebindingrequest/annotations/bindinginfo.go
@@ -8,6 +8,7 @@ import (
 const (
 	// ServiceBindingOperatorAnnotationPrefix is the prefix of Service Binding Operator related annotations.
 	ServiceBindingOperatorAnnotationPrefix = "servicebindingoperator.redhat.io/"
+	ServiceBindingSpecAnnotationPrefix     = "servicebinding.dev/"
 )
 
 // BindingInfo represents the pieces of a binding as parsed from an annotation.
@@ -21,6 +22,14 @@ type BindingInfo struct {
 	Descriptor string
 	// Value is the original annotation value.
 	Value string
+	// Value to be fetched from a secret/ConfigMap
+	ObjectType string
+	// Extract a specific field from the configmap/Secret from the Kubernetes resource and map it to different name in the binding Secret
+	SourceKey string
+	// Extract a specific field from the configmap/Secret from the Kubernetes resource
+	VarReference string
+	// Specifies if the element is to be bound as an environment variable or a volume mount using the keywords envVar and volume
+	BindAs string // by default should be an envVar
 }
 
 var ErrInvalidAnnotationPrefix = errors.New("invalid annotation prefix")
@@ -30,29 +39,60 @@ var ErrEmptyAnnotationName = errors.New("empty annotation name")
 // NewBindingInfo parses the encoded in the annotation name, returning its parts.
 func NewBindingInfo(name string, value string) (*BindingInfo, error) {
 	// do not process unknown annotations
-	if !strings.HasPrefix(name, ServiceBindingOperatorAnnotationPrefix) {
-		return nil, ErrInvalidAnnotationPrefix
+	if strings.HasPrefix(name, ServiceBindingOperatorAnnotationPrefix) {
+
+		cleanName := strings.TrimPrefix(name, ServiceBindingOperatorAnnotationPrefix)
+		if len(cleanName) == 0 {
+			return nil, ErrEmptyAnnotationName
+		}
+
+		parts := strings.SplitN(cleanName, "-", 2)
+
+		resourceReferencePath := parts[0]
+		sourcePath := parts[0]
+
+		// the annotation is a reference to another manifest
+		if len(parts) == 2 {
+			sourcePath = parts[1]
+		}
+
+		return &BindingInfo{
+			ResourceReferencePath: resourceReferencePath,
+			SourcePath:            sourcePath,
+			Descriptor:            strings.Join([]string{value, sourcePath}, ":"),
+			Value:                 value,
+		}, nil
+	}
+	if strings.HasPrefix(name, ServiceBindingSpecAnnotationPrefix) {
+
+		// "servicebinding.dev/dbCredentials": "path={.status.data.dbCredentials},objectType=Secret"
+
+		split := func(r rune) bool {
+			return r == '=' || r == ','
+		}
+		a := strings.FieldsFunc(value, split)
+		m := map[string]string{}
+
+		for i := 0; i < len(a); i = i + 2 {
+
+			k := a[i]
+			val := a[i+1]
+			m[k] = val
+		}
+		varReference := strings.TrimPrefix(name, ServiceBindingSpecAnnotationPrefix)
+		// This will contain dbCredentials(entire secret reference), can be a specific data key from secret/CM
+		if len(varReference) == 0 {
+			return nil, ErrEmptyAnnotationName
+		}
+
+		return &BindingInfo{
+			SourcePath:   m["path"],
+			ObjectType:   m["objectType"],
+			SourceKey:    m["sourceKey"],
+			VarReference: varReference,
+			Value:        value,
+		}, nil
 	}
 
-	cleanName := strings.TrimPrefix(name, ServiceBindingOperatorAnnotationPrefix)
-	if len(cleanName) == 0 {
-		return nil, ErrEmptyAnnotationName
-	}
-
-	parts := strings.SplitN(cleanName, "-", 2)
-
-	resourceReferencePath := parts[0]
-	sourcePath := parts[0]
-
-	// the annotation is a reference to another manifest
-	if len(parts) == 2 {
-		sourcePath = parts[1]
-	}
-
-	return &BindingInfo{
-		ResourceReferencePath: resourceReferencePath,
-		SourcePath:            sourcePath,
-		Descriptor:            strings.Join([]string{value, sourcePath}, ":"),
-		Value:                 value,
-	}, nil
+	return nil, ErrInvalidAnnotationPrefix
 }
